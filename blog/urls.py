01ae6c5bfc7dc00888e2cdb08ce27695ee5a663f
io-23SklyarAnton <80kap.i.toshka@gmail.com>
2024-02-08 15:06:29 +0200

    added views and basic templates to view posts on the web site

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
+from . import views
 
+app_name = 'blog'
 
-urlpatterns = []
+urlpatterns = [path('', views.all_post_list, name='all_posts'),
+               path('<int:post_id>/', views.post_detail, name='post_detail'),
+               ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+
+def all_post_list(request):
+    posts = Post.published.all()
+
+    return render(request, 'post-list.html', {'posts': posts})
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+
+    return render(request, 'post-detail.html', {'post': post})
