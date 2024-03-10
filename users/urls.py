3a507ea7966984be901f99377d3d156c451fdedf
io-23SklyarAnton <80kap.i.toshka@gmail.com>
2024-03-10 23:43:42 +0200

    added registration

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,5 @@ app_name = 'users'
 
 urlpatterns = [path('login/', views.Login.as_view(), name='login'),
                path('logout/', views.Logout.as_view(), name='logout'),
+               path('registration/', views.Registration.as_view(), name='registration'),
                ]
