722c9420fa8e295ba2f2c53a113111de2613aace
io-23SklyarAnton <80kap.i.toshka@gmail.com>
2024-03-11 12:13:14 +0200

    added recent posts and popular posts list to the side bar

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,6 +1,7 @@
 from django import template
 from django.template.defaultfilters import stringfilter
 from autocorrect import Speller
+from django.db.models import Count
 
 register = template.Library()
 
@@ -10,3 +11,17 @@ register = template.Library()
 def autocorrect(value):
     spell = Speller()
     return spell(value)
+
+
+@register.inclusion_tag('blog/includes/recent-posts.html')
+def recent_posts(value=3):
+    from ..models import Post
+    posts = Post.published.order_by('-publish')[:value]
+    return {'posts': posts}
+
+
+@register.inclusion_tag('blog/includes/popular-posts.html')
+def popular_posts(value=3):
+    from ..models import Post
+    posts = Post.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:value]
+    return {'posts': posts}
