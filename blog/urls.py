93f44fe658ee1b97bf9e1b31487ad3cb73697094
io-23SklyarAnton <80kap.i.toshka@gmail.com>
2024-03-15 15:06:07 +0200

    added RSS feed for all posts on the site

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 import os
+from blog.services.feed import LatestPostsFeed
 
 app_name = 'blog'
 concrete_post_url = '<int:pk>/<slug:post_slug>/'
@@ -14,5 +15,6 @@ urlpatterns = [
     path(os.path.join('user_post/', concrete_post_url), views.UserPostDetail.as_view(), name='user_post_detail'),
     path(concrete_post_url, views.PostDetail.as_view(), name='post_detail'),
     path('create_post/', views.PostCreate.as_view(), name='create_post'),
-    path(os.path.join('edit_post/', concrete_post_url), views.PostEdit.as_view(), name='edit_post')
+    path(os.path.join('edit_post/', concrete_post_url), views.PostEdit.as_view(), name='edit_post'),
+    path('feed/', LatestPostsFeed(), name='post_feed')
 ]
