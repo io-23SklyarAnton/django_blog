c87783cb3d64325172bf489a5f52c4448d9d714b
io-23SklyarAnton <80kap.i.toshka@gmail.com>
2024-03-24 21:10:54 +0200

    added possibility to change and reset user password

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = 'users'
 
@@ -7,5 +8,12 @@ urlpatterns = [
     path('user_profile/', views.ProfileView.as_view(), name='user_profile'),
     path('logout/', views.Logout.as_view(), name='logout'),
     path('registration/', views.Registration.as_view(), name='registration'),
+    path('change_password/', views.PasswordChange.as_view(), name='change_password'),
+    path('change_password_done/', views.PasswordChangeDone.as_view(), name='change_password_done'),
+    path('reset_password/', views.PasswordReset.as_view(), name='reset_password'),
+    path('password_reset_done/', views.PasswordResetDone.as_view(), name='reset_password_done'),
+    path('reset_password_confirm/<uidb64>/<token>/', views.PasswordResetConfirm.as_view(),
+         name='reset_password_confirm'),
+    path('reset_password_complete/', views.PasswordResetComplete.as_view(), name='reset_password_complete'),
     path('', include('django.contrib.auth.urls')),
 ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.models import User
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LogoutView, PasswordChangeView, PasswordResetView, PasswordChangeDoneView, \
+    PasswordResetConfirmView, PasswordResetDoneView, PasswordResetCompleteView
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, FormView, UpdateView
+from django.views.generic import CreateView, UpdateView
 
-from .forms import RegistrationForm, UserForm
+from .forms import RegistrationForm
 
 
 class Logout(LogoutView):
@@ -19,17 +20,6 @@ class Registration(CreateView):
     success_url = reverse_lazy('users:login')
 
 
-# class ProfileView(FormView):
-#     template_name = 'user/profile.html'
-#     form_class = UserForm
-#     success_url = '#'
-#
-#     def get_form_kwargs(self):
-#         """adds User instance arg to form"""
-#         kwargs = super().get_form_kwargs()
-#         kwargs['instance'] = self.request.user
-#         return kwargs
-
 class ProfileView(UpdateView):
     template_name = 'user/profile.html'
     model = User
@@ -41,3 +31,32 @@ class ProfileView(UpdateView):
     def form_valid(self, form):
         form.save()
         return render(self.request, self.template_name, {'success': True, 'form': form})
+
+
+class PasswordChange(PasswordChangeView):
+    template_name = 'registration/change_password.html'
+    success_url = reverse_lazy('users:change_password_done')
+
+
+class PasswordChangeDone(PasswordChangeDoneView):
+    template_name = 'registration/change_password_done.html'
+
+
+class PasswordReset(PasswordResetView):
+    template_name = 'registration/reset_password.html'
+    email_template_name = 'registration/reset_password_email.html'
+    success_url = reverse_lazy('users:reset_password_done')
+    extra_email_context = {'domain': '127.0.0.1:8000', 'site_name': '127.0.0.1:8000'}
+
+
+class PasswordResetConfirm(PasswordResetConfirmView):
+    template_name = 'registration/reset_password_confirm.html'
+    success_url = reverse_lazy("users:reset_password_complete")
+
+
+class PasswordResetDone(PasswordResetDoneView):
+    template_name = 'registration/reset_password_done.html'
+
+
+class PasswordResetComplete(PasswordResetCompleteView):
+    template_name = 'registration/reset_password_complete.html'
