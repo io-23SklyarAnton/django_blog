4a96637121241eb40fb1617f587095e8c88d712c
io-23SklyarAnton <80kap.i.toshka@gmail.com>
2024-04-02 22:53:59 +0300

    changed users avatar 'upload_to' path

--- users/models.py
+++ users/models.py
@@ -2,9 +2,13 @@ from django.contrib.auth.models import AbstractUser, User
 from django.db import models
 
 
+def user_dir_path(instance, filename):
+    return f"users/user_{instance.username}/{filename}"
+
+
 class User(AbstractUser):
     date_of_birth = models.DateField(blank=True, null=True)
-    avatar = models.ImageField(upload_to='users/%Y%m%d/', default='users/default_avatar.png')
+    avatar = models.ImageField(upload_to=user_dir_path, default='users/default_avatar.png')
 
     class Meta(AbstractUser.Meta):
         swappable = "AUTH_USER_MODEL"
